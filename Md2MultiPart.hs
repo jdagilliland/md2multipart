73cef4728c9090ce21794e0eb18b8fdb89833160
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2015-06-27 21:35:25 -0400

    Separate out main logic into function

--- Md2MultiPart.hs
+++ Md2MultiPart.hs
@@ -1,13 +1,18 @@
 import System.Environment
 import Data.Either.Unwrap
 import Text.Pandoc
+import Text.Pandoc.Error
 
 -- | 'main' runs the main program
 main :: IO ()
-main = print pandocConverted
+main = print . fromRight $ md2html textToConvert
 
+md2html :: String -> Either PandocError String
+md2html inText = do
+    pandocParsed <- readMarkdown def inText
+    return $ writeHtmlString def pandocParsed
+
+textToConvert :: String
 textToConvert = unlines [ "Hello World.\n"
                         , "    this is a Markdown code block\n"
                         , "[This is a link](http://www.latermuse.com/)\n" ]
-pandocParsed = readMarkdown def textToConvert
-pandocConverted = writeHtmlString def $ fromRight pandocParsed
