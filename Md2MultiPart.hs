c13c56323edb238f51fd2225849232b80845b886
Jason Daniel Augustine Gilliland <jdagilliland@gmail.com>
2015-06-27 21:39:14 -0400

    Convert markdown stdin to HTML

--- Md2MultiPart.hs
+++ Md2MultiPart.hs
@@ -5,14 +5,12 @@ import Text.Pandoc.Error
 
 -- | 'main' runs the main program
 main :: IO ()
-main = print . fromRight $ md2html textToConvert
+main = do
+    textToConvert <- getContents
+    print . fromRight $ md2html textToConvert
 
 md2html :: String -> Either PandocError String
 md2html inText = do
     pandocParsed <- readMarkdown def inText
     return $ writeHtmlString def pandocParsed
 
-textToConvert :: String
-textToConvert = unlines [ "Hello World.\n"
-                        , "    this is a Markdown code block\n"
-                        , "[This is a link](http://www.latermuse.com/)\n" ]
